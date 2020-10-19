e2d074ec21e27c2c661bce423e34c4a68ff8aae2
Game Over <janessarefong@gmail.com>
2020-10-18 18:54:21 -0700

    rename sql file, pseudocode and add console.table package

---
+++ server.js
@@ -0,0 +1,38 @@
+/*
+three main tables:
+1. by department
+    - id (primary key)
+    - department name
+2. by role
+    - id (primary key)
+    - role title
+    - salary in decimal
+    - department_id -- reference to dept role belonging to
+3. by employee
+    - id (primary key)
+    - first name
+    - last name
+    - role_id -- reference to employee role
+    - manager_id -- reference to manager, can be null if employee has no manager
+
+Features:
+1. add dempartment, role, employee
+2. view department, role, employee
+3. update employee roles
+
+Additional features:
+1. update employee managers
+2. view employees by manager
+3. delete departments, roles, and employees
+4. view total utilized budget of a department -- combined salaries in the dept
+
+Steps to create:
+1. install dependencies mysql and inquirer and console.table
+2. create connection 
+3. build mysql starter template table in seed.sql
+3. create prompts for each table
+*/
+
+// dependencies
+const sql = require("mysql");
+const inquirer = require("inquirer");
