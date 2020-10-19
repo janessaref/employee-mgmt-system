f3e7fc70041a4ce3be1141a787e016e3421de183
Game Over <janessarefong@gmail.com>
2020-10-18 18:59:00 -0700

    add connection

--- server.js
+++ server.js
@@ -36,3 +36,18 @@ Steps to create:
 // dependencies
 const sql = require("mysql");
 const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "codIng9!",
+    database: "employeeMgmt_DB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
